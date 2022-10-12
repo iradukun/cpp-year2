3d99de31d78cb971779504facfc336f1ceb3ebcd
iradukun <mustaphairadukunda@gmail.com>
2022-10-12 20:47:51 +0200

    something

--- btwise/lesson1.cpp
+++ btwise/lesson1.cpp
@@ -4,15 +4,10 @@ int main(){
   int a=12;
   int b=25;
   cout<<"(a &b ) "<<(a&b)<<endl;
-  cout<<"(a << b ) "<<(a&b)<<endl;
-  cout<<"(a &b ) "<<(a&b)<<endl;
- 
-
+  cout<<"(a << 1 ) "<<(a<<1)<<endl;
+  cout<<"(a >>b ) "<<(a>>1)<<endl;
   cout<<"(a |b ) "<<(a|b)<<endl;
   cout<<"(a ^b ) "<<(a^b)<<endl;
   cout<<"(a ^(~b) ) "<<(a^(~b))<<endl;
-
-
-
-    return 0;
+   return 0;
 }
